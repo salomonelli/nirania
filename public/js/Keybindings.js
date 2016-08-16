229cbd111db9cb56b4e15ec632943ddcec8b2ad6
System Administrator <sara.steiert@outlook.com>
2016-08-16 23:26:47 +0200

    added jumping

--- public/js/Keybindings.js
+++ public/js/Keybindings.js
@@ -23,8 +23,9 @@ module.exports = (function ($) {
             case 68:
                 return 'right';
                 break;
-            case 32:
+            //case 32:
             case 87:
+            case 38:
                 return 'up';
                 break;
             default:
