91a22c53ee1165d4ceeec93f4df16af2068bca68
Jan-Philipp <jan-philipp_erdmann@web.de>
2016-08-17 14:21:31 +0200

    package

--- public/js/protagonist/CollisionDetector.js
+++ public/js/protagonist/CollisionDetector.js
@@ -1,4 +1,4 @@
-module.exports = (function(){
+module.exports = (function(THREE){
     function CollisionDetector(mesh, obstacles, scene){
         this.mesh = mesh;
         this.obstacles = obstacles;
@@ -6,6 +6,26 @@ module.exports = (function(){
 
     }
 
+    CollisionDetector.prototype.detectCollision = function () {
+
+        var vector = new THREE.Vector3(0, -1, 0);
+        var rayCaster = new THREE.Raycaster(this.mesh.position, vector);
+        var forbiddenZones = [];
+
+        for(var i = 0; i < this.obstacles.length < 1; i++){
+
+            forbiddenZones.push(this.obstacles[i]);
+
+
+        }
+        co
+        var intersects = rayCaster.intersectObjects(forbiddenZones);
+
+    }
 
     return CollisionDetector;
-})();
+})(
+
+        require('three')
+
+);
