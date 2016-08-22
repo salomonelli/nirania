1bf5b375b70c09d2ee03fc118cbc01d7e8bf7003
System Administrator <sara.steiert@outlook.com>
2016-08-22 18:23:30 +0200

    changed colors, created palette

--- public/js/COLOR.js
+++ public/js/COLOR.js
@@ -1,11 +1,27 @@
-module.exports = (function(){
+module.exports = (function() {
     var COLOR = {
-        background: 0xFF7864,
-        particles: 0xffffff,//FD5C98
-        way: 0xDFD1E6,//0xCEBCA5,0x2598BA, //
+        background: 0x68c3c0,
+        particles: 0xffffff, //FD5C98
+        way: 0xE9DCB9, //0xCEBCA5,0x2598BA, //
         protagonist: {
             body: 0xffffff,
             head: 0xffffff
+        },
+        ring: 0xF55A46,
+        box: 0xE9DCB9,
+        basic: {
+            red: 0xf25346,
+            white: 0xd8d0d1,
+            brown: 0x59332e,
+            pink: 0xF5986E,
+            brownDark: 0x23190f,
+            blue: 0x68c3c0,
+            beige: 0xE9DCB9,
+            orange: 0xF55A46,
+            grey: 0xD9D0D1,
+            lila: 0x311B92,
+            pink2: 0xFBAAB5,
+            green: 0x33691E
         }
     };
     return COLOR;
