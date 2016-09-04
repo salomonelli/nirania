9631e9efb558a8d9c93fd0486b049ce4758736fa
salamimitpizza <sara.steiert@outlook.com>
2016-09-04 22:34:43 +0200

    fixed run.bash

--- run.bash
+++ run.bash
@@ -1,9 +1,14 @@
+#!/usr/bin/env bash
+
+SCRIPT="`readlink -e $0`"
+SCRIPTPATH="`dirname $SCRIPT`"
+echo $SCRIPTPATH
+
 NAME="nirania"
-CWD=$(pwd)
 docker stop $NAME
 docker rm $NAME
 docker rmi --force $NAME
 docker build -t $NAME .
-docker run -it -p 80:80 \
--v $CWD/app:/app \
+docker run -itd -p 18787:80 \
+-v $SCRIPTPATH/app:/app \
 --name $NAME $NAME
