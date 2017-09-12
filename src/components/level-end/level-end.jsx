eec8d94caa3ed08d0012c11127e9d600140aefdd
salomonelli <sara.steiert@outlook.com>
2017-09-12 22:52:05 +0200

    FIX end and beginning of levels

--- src/components/level-end/level-end.jsx
+++ src/components/level-end/level-end.jsx
@@ -36,23 +36,25 @@ class LevelEndComponent extends Component {
 
     render() {
         return (
-            <div className='level-end'>
-                <div className='level-repeat' onClick={this.props.onRepeat}>
-                  <ReplayIcon></ReplayIcon>
-                  <div>Repeat</div>
-                </div>
-                <div className={'level-next ' + (this.props.success ? '' : 'disabled')} onClick={this.props.onNextLevel}>
-                  <SkipNextIcon></SkipNextIcon>
-                  <div>Level {this.props.nextLevel}</div>
-                </div>
-                <div className='level-list' onClick={this.props.onAllLevels}>
-                  <ViewListIcon></ViewListIcon>
-                  <div>Overview</div>
-                </div>
-                <div className='auto-starter'>
-                  <div>{
-                      this.props.success ? 'Next level starting in ' : 'Repeating level in '
-                    } {this.state.timer}s</div>
+            <div >
+              <div className='level-end'>
+                  <div className='level-repeat' onClick={this.props.onRepeat}>
+                    <ReplayIcon></ReplayIcon>
+                    <div>Repeat</div>
+                  </div>
+                  <div className={'level-next ' + (this.props.success ? '' : 'disabled')} onClick={this.props.onNextLevel}>
+                    <SkipNextIcon></SkipNextIcon>
+                    <div>Level {this.props.nextLevel}</div>
+                  </div>
+                  <div className='level-list' onClick={this.props.onAllLevels}>
+                    <ViewListIcon></ViewListIcon>
+                    <div>Overview</div>
+                  </div>
+              </div>
+              <div className='auto-starter'>
+                <div>{
+                    this.props.success ? 'Next level starting in ' : 'Repeating level in '
+                  } {this.state.timer}s</div>
                 </div>
             </div>
         );
--- src/pages/level/level.jsx
+++ src/pages/level/level.jsx
@@ -29,31 +29,34 @@ class LevelPage extends Component {
             playing: false,
             end: false,
             canNotBePlayed: false,
+            autostart: true,
             success: false,
             survived: false,
             diamonds: 0,
             nextLevel: null,
-            autostart: false,
             gameFrameComponent: () => <GameFrameComponent ref={instance => this.gameFrameComponent = instance} level={this.props.match.params.level}/>
         });
         await this.componentDidMount();
     }
 
     async componentDidMount() {
-        this.setState({autostart: this.props.location.search.replace('?', '')});
+        if (!!this.state.autostart) this.setState({autostart: this.props.location.search.replace('?', '')});
         this.levelModel = await LevelModelGet();
         const levelNr = this.props.match.params.level;
         this.setState({nextLevel: parseInt(levelNr, 10) + 1});
         this.levelDoc = await this.levelModel.getByNr(levelNr);
         const canBePlayed = await this.levelDoc.canBePlayed();
         if (!canBePlayed) this.setState({canNotBePlayed: true});
-        if(this.state.autostart) await this.play();
-        else {
-            Rx.Observable.fromEvent(document, 'keydown')
-            .filter(() => this.state.playing === false)
-            .first()
-            .subscribe(() => this.play());
-        }
+        else await this.play();
+      /*
+      if(this.state.autostart) await this.play();
+      else {
+          Rx.Observable.fromEvent(document, 'keydown')
+          .filter(() => this.state.playing === false)
+          .first()
+          .subscribe(() => this.play());
+      }
+       */
     }
 
     async play() {
@@ -74,7 +77,6 @@ class LevelPage extends Component {
                 currentValue.diamonds
             );
             await Promise.all([promise1, promise2]);
-            console.dir(this.levelEndComponent);
             this.levelEndComponent.startCountDown();
             this.setState({end: true});
             this.setState({success: currentValue.success});
@@ -84,7 +86,7 @@ class LevelPage extends Component {
     }
 
     async playNextLevel() {
-        this.props.history.push('/level/'+this.state.nextLevel);
+        this.props.history.push('/level/'+ this.state.nextLevel);
         await this.reset();
     }
     allLevels() {
@@ -104,21 +106,14 @@ class LevelPage extends Component {
                   <h1>can not be played</h1>
                 </div>
               }
-              {!this.state.canNotBePlayed && !this.state.playing && !this.state.autostart &&
-                <div className="intro">
-                  <h1>Nirania</h1>
-                  <h3>Level {this.props.match.params.level}</h3>
-                  <RaisedButton label="Play" primary={true} onClick={this.play.bind(this)}/>
-                </div>
-              }
               <LevelDashboardComponent diamonds={this.state.diamonds}/>
               <DividerComponent ref={instance => this.dividerComponent = instance}/>
               <CurrentGameFrame />
-                  <div className={'intro' + (this.state.end ? '' : ' hidden')}>
-                      <LevelEndComponent ref={instance => this.levelEndComponent = instance} nextLevel={this.state.nextLevel} success={this.state.success}
-                        onAllLevels={this.allLevels.bind(this)} onNextLevel={this.playNextLevel.bind(this)}
-                        onRepeat={this.reset.bind(this)}/>
-                  </div>
+              <div className={'intro' + (this.state.end ? '' : ' hidden')}>
+                <LevelEndComponent ref={instance => this.levelEndComponent = instance} nextLevel={this.state.nextLevel} success={this.state.success}
+                  onAllLevels={this.allLevels.bind(this)} onNextLevel={this.playNextLevel.bind(this)}
+                  onRepeat={this.reset.bind(this)}/>
+              </div>
             </div>
           </MuiThemeProvider>
         );
@@ -141,4 +136,11 @@ this.state.success &&
 onClick={this.playNextLevel.bind(this)}/>
 
 }
+
+
+<div className="intro">
+  <h1>Nirania</h1>
+  <h3>Level {this.props.match.params.level}</h3>
+  <RaisedButton label="Play" primary={true} onClick={this.play.bind(this)}/>
+</div>s
  */
